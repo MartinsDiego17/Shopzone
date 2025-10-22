2ec261b5d83baf6c0dba5428df90703ba77f8dde
MartinsDiego17 <martinsdiego0513@outlook.es>
2025-10-21 23:00:38 -0300

    add linkedin link on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import { SectionList } from "../sections-list/SectionsList";
 import { Title } from "../Title/Title";
 import "./footer.css";
+import { Link } from "react-router";
 
 export const Footer = ({ cant_products }) => {
 
@@ -10,7 +11,10 @@ export const Footer = ({ cant_products }) => {
             <SectionList cant_products={cant_products} />
             <p className="email">shopzone@hotmail.com</p>
             <hr />
-            <p>@ 2025. Todos los derechos reservados. <br /> Desarrollado y diseñado por Diego Martins.</p>
+            <p>
+                @ 2025. Todos los derechos reservados. <br /> Desarrollado y diseñado por
+                <Link to={"https://www.linkedin.com/in/diego-martins-563954278/"} target="_blank"> Diego Martins</Link>
+            </p>
         </footer>
     )
 }
